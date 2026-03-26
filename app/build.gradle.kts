c1a9194af2b590fb38dfae32596168d8c01206ef
ai_assistant <zbin@soeasy960.com>
2026-03-26 15:31:52 +0800

    bump versionCode to 13 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,7 +89,7 @@ android {
         applicationId = "com.bluearcyiji"
         minSdk = 24
         targetSdk = 36
-        versionCode = 12
+        versionCode = 13
         versionName = "1.0"
 
         buildConfigField("String", "API_BASE_URL", "\"$apiBaseUrl\"")
